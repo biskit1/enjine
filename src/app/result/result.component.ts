9da803b010782809bbaba32013e0954b6ce146fd
biskit1 <chayadanz@gmail.com>
2019-03-18 15:56:50 -0400

    chart implementation

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Result, FileInfo } from '../result';
+import { Result } from '../result';
 import { ResultsService } from '../results.service'
 import { Router } from '@angular/router';
 
@@ -10,21 +10,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
+
   // used to differentiate between bigger and smaller file for rendering
   smaller: Result[] = [];
   bigger: Result[] = [];
 
-  // filenames
-  // fileName1:string;
-  // fileName2:string;
-
-  // file information
-  fileInfo: FileInfo[];
-  fileInfo1: FileInfo;
-  fileInfo2:FileInfo;
-
   constructor(private service: ResultsService, private router: Router) {
-  
   }
 
   ngOnInit() {
@@ -33,19 +24,10 @@ export class ResultComponent implements OnInit {
     smObs.subscribe((small: Result[]) => {
       this.smaller = small;
     });
-    
+
     const bigObs = this.service.getBig();
     bigObs.subscribe((big: Result[]) => {
       this.bigger = big;
     });
-
-    // get array containing general file inforation
-    this.fileInfo = this.service.getReuslts();
-
-    // extract individual file information to store seperately for rendering 
-    this.fileInfo1= this.fileInfo[0];
-    this.fileInfo2 = this.fileInfo[1]
-    // this.fileName1 = this.fileInfo1.etfName;
-    // this.fileName2 = this.fileInfo2.etfName;
   }
 }
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -9,11 +9,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
-
   // final array of user-selected files to be sent to service worker
-  selectedFiles:File[] = [];
+  selectedFiles: File[] = [];
 
-  
   constructor(private service: ResultsService, private router: Router) { }
 
   ngOnInit() {
@@ -21,14 +19,24 @@ export class UploadComponent implements OnInit {
 
   // save user-selected files locally to handle file removal/deletion before sending to parse 
   processFile(fileInput: any) {
-    for (var i = 0; i < fileInput.files.length; i++){
+    for (var i = 0; i < fileInput.files.length; i++) {
       this.selectedFiles.push(fileInput.files[i]);
     }
   }
 
   // send final selected files to service worker for parsing
-  uploadFiles(files: File[]) {
+  uploadFiles() {
     this.service.uploadFiles(this.selectedFiles);
+  }
+
+  // check that uploaded file ends with appropriate file extention 
+  goodFile() {
+    if (this.selectedFiles[0].name.endsWith(".csv") && this.selectedFiles[1].name.endsWith(".csv")) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 }
 
